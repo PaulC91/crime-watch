f1dc7341ee4279132dbe0e161b5d98dffc723522
Paul Campbell <pacampbell91@gmail.com>
2019-01-30 18:37:00 +0000

    use fct_lump for legend

--- app.R
+++ app.R
@@ -34,22 +34,24 @@ ui <- bootstrapPage(
                   }
                 });
                 ')
-    ),
+  ),
   
   leafletOutput("map", width = "100%", height = "100%"),
   
-  absolutePanel(top = 10, right = 10, style = "z-index:500; text-align: right;",
-                tags$h2("Culture of Insight's Crime Watch"),
-                tags$a("About this tool", href="https://cultureofinsight.com/portfolio/crimewatch/")
+  absolutePanel(
+    top = 10, right = 10, style = "z-index:500; text-align: right;",
+    tags$h2("Culture of Insight's Crime Watch"),
+    tags$a("About this tool", href="https://cultureofinsight.com/portfolio/crimewatch/")
   ),
   
-  absolutePanel(top = 100, left = 10, draggable = TRUE, width = "20%", style = "z-index:500; min-width: 300px;",
-                textInput("geocode", "Type an address or location", placeholder = "in England, Wales or NI"),
-                checkboxInput("use_location", "Or use your current location?"),
-                actionButton("go", "Find Crime!", class = "btn-primary"),
-                highchartOutput("selectstat")
+  absolutePanel(
+    top = 100, left = 10, draggable = TRUE, width = "20%", style = "z-index:500; min-width: 300px;",
+    textInput("geocode", "Type an address or location", placeholder = "in England, Wales or NI"),
+    checkboxInput("use_location", "Or use your current location?"),
+    actionButton("go", "Find Crime!", class = "btn-primary"),
+    highchartOutput("selectstat")
   )
-  )
+)
 
 server <- function(input, output, session) {
   
@@ -61,103 +63,98 @@ server <- function(input, output, session) {
   
   observeEvent(input$go, {
     
-    withProgress(message = 'Fetching data from data.police.uk...',
-                 value = 1/5, {
-                   
-                   if (input$use_location) {
-                     
-                     validate(
-                       need(input$geolocation, message = FALSE)
-                     )
-                     
-                     lat <- input$lat
-                     long <- input$long
-                     
-                     place <- opencage_reverse(lat, long, countrycode = "GB")
-                     place <- as.character(place$results$components.postcode[1])
-                     
-                   } else {
-                     
-                     validate(
-                       need(nchar(input$geocode > 2), message = FALSE)
-                     )
-                     
-                     geoc <- opencage_forward(placename = input$geocode, countrycode = "GB")
-                     
-                     lat <- geoc$results$geometry.lat[1]
-                     long <- geoc$results$geometry.lng[1]
-                     
-                     place = input$geocode
-                     
-                   }
-                   
-                   incProgress(1/5)
-                   
-                   tryCatch({
-                     crime <- ukp_crime(lat, long) %>% 
-                       mutate(date = as.Date(paste0(date, "-01"))) %>% 
-                       mutate(date = format(date, format = "%B %Y"))
-                     
-                     crime_rank <- crime %>% 
-                       count(category) %>% 
-                       arrange(desc(n))
-                     
-                     crime <- crime %>% 
-                       mutate(
-                         top5 = case_when(
-                           category %in% crime_rank$category[1:5] ~ category,
-                           TRUE ~ "hover for detail"),
-                         top5 = factor(top5, levels = c(crime_rank$category[1:5], "hover for detail"))
-                         )
-                   },
-                   error = function(e) {
-                     crime <- NULL
-                   }
-                   )
-                   
-                   
-                   incProgress(1/5)
-                   
-                   tryCatch({
-                     pal <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02")
-                     leafPal <- colorFactor(pal, levels = c(crime_rank$category[1:5], "hover for detail"))
-                     #nord::nord("aurora", 6, reverse = TRUE)
-                     
-                     leafletProxy("map", data = crime) %>%
-                       setView(long, lat, zoom = 14) %>% 
-                       clearShapes() %>% 
-                       clearControls() %>% 
-                       addCircles(~long, ~lat, stroke = FALSE, fill = TRUE, fillOpacity = .7, 
-                                  color = ~leafPal(top5), label = ~category, radius = 30) %>% 
-                       addLegend("bottomright", pal = leafPal, values = ~top5, title = "Category")
-                     
-                     incProgress(1/5)
-                     
-                     output$selectstat <- renderHighchart({
-                       
-                       hchart(crime_rank, "bar", hcaes(category, n)) %>% 
-                         hc_colors("SteelBlue") %>% 
-                         hc_title(text = paste("Crimes within 1 mile of", isolate(place))) %>% 
-                         hc_subtitle(text = unique(crime$date)) %>% 
-                         hc_xAxis(title = list(text = "")) %>% 
-                         hc_yAxis(title = list(text = "Incidents")) %>%
-                         hc_legend(enabled = FALSE) %>% 
-                         hc_add_theme(hc_theme_smpl()) %>% 
-                         hc_chart(backgroundColor = "transparent")
-                       
-                     })
-                   
-                   },
-                   error = function(e) {
-                     showModal(modalDialog(title = "Sorry!", 
-                                           tags$p("We couldn't find any data for that location."),
-                                           tags$p("Give another one a try!")))
-                   }
-                   )
-                   
-                   incProgress(1/5)
-                   
-                 })
+    withProgress(
+      message = 'Fetching data from data.police.uk...',
+      value = 1/5, {
+        
+        if (input$use_location) {
+          
+          validate(
+            need(input$geolocation, message = FALSE)
+          )
+          
+          lat <- input$lat
+          long <- input$long
+          
+          place <- opencage_reverse(lat, long, countrycode = "GB")
+          place <- as.character(place$results$components.postcode[1])
+          
+        } else {
+          
+          validate(
+            need(nchar(input$geocode > 2), message = FALSE)
+          )
+          
+          geoc <- opencage_forward(placename = input$geocode, countrycode = "GB")
+          
+          lat <- geoc$results$geometry.lat[1]
+          long <- geoc$results$geometry.lng[1]
+          
+          place = input$geocode
+          
+        }
+        
+        incProgress(1/5)
+        
+        tryCatch({
+          crime <- ukp_crime(lat, long) %>% 
+            mutate(date = as.Date(paste0(date, "-01"))) %>% 
+            mutate(date = format(date, format = "%B %Y")) %>% 
+            mutate(top5 = fct_lump(factor(category), n = 5, other_level = "hover-for-detail"))
+          
+          crime_rank <- crime %>%
+            count(category) %>%
+            arrange(desc(n))
+          
+        },
+        error = function(e) {
+          crime <- NULL
+        }
+        )
+        
+        incProgress(1/5)
+        
+        tryCatch({
+          pal <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02")
+          leafPal <- colorFactor(pal, crime$top5)
+          
+          leafletProxy("map", data = crime) %>%
+            setView(long, lat, zoom = 14) %>% 
+            clearShapes() %>% 
+            clearControls() %>% 
+            addCircles(~long, ~lat, stroke = FALSE, fill = TRUE, fillOpacity = .7, 
+                       color = ~leafPal(top5), label = ~category, radius = 30) %>% 
+            addLegend("bottomright", pal = leafPal, values = ~top5, title = "Category")
+          
+          incProgress(1/5)
+          
+          output$selectstat <- renderHighchart({
+            
+            hchart(crime_rank, "bar", hcaes(category, n)) %>% 
+              hc_colors("SteelBlue") %>% 
+              hc_title(text = paste("Crimes within 1 mile of", isolate(place))) %>% 
+              hc_subtitle(text = unique(crime$date)) %>% 
+              hc_xAxis(title = list(text = ""), gridLineWidth = 0, minorGridLineWidth = 0) %>% 
+              hc_yAxis(title = list(text = "Incidents"), gridLineWidth = 0, minorGridLineWidth = 0) %>%
+              hc_legend(enabled = FALSE) %>% 
+              hc_tooltip(pointFormat = "Incidents: <b>{point.y}</b>") %>% 
+              hc_plotOptions(series = list(cursor = "default")) %>% 
+              hc_add_theme(hc_theme_smpl()) %>% 
+              hc_chart(backgroundColor = "transparent")
+            
+          })
+          
+        },
+        error = function(e) {
+          showModal(modalDialog(title = "Sorry!", 
+                                tags$p("We couldn't find any data for that location."),
+                                tags$p("Give another one a try!")))
+        }
+        )
+        
+        incProgress(1/5)
+        
+      })
   })
   
   
